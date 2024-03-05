8af871de8b66b190706191ec73d46563535996df
quynh-nta <nguyenthianhquynh116@gmail.com>
2024-03-05 23:36:29 +0700

    Initial Commit

---
+++ LearningOpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearningOpenGL
+//
+//  Created by Nguyen Thi Anh Quynh on 3/5/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
